0129f8529978b4b3dba278a18d66d955bd36433f
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2020-11-04 15:34:28 -0600

    get user github profile and repos

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const request = require('request');
+const config = require('config');
 const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
 
@@ -223,4 +225,140 @@ router.put(
    }
 );
 
+// @route       DELETE api/profile/experience/:exp_id
+// @desc        Delete experience from user profile
+// @access      Private
+
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+   try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      // Get the remove index
+      const removeIndex = profile.experience
+         .map((item) => item.id)
+         .indexOf(req.params.exp_id);
+
+      profile.experience.splice(removeIndex, 1);
+
+      await profile.save();
+
+      res.json(profile);
+   } catch (err) {
+      console.error(err);
+      res.status(500).send('Server Error');
+   }
+});
+
+// @route       PUT api/profile/education
+// @desc        Add education to a users profile
+// @access      Private
+
+router.put(
+   '/education',
+   [
+      auth,
+      [
+         check('school', 'School is required').not().isEmpty(),
+         check('degree', 'Degree is required').not().isEmpty(),
+         check('fieldofstudy', 'Field of Study is required').not().isEmpty(),
+         check('from', 'Please enter a "from" date').not().isEmpty(),
+      ],
+   ],
+   async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+         return res.status(400).json({ errors: errors.array() });
+      }
+
+      const {
+         school,
+         degree,
+         fieldofstudy,
+         from,
+         to,
+         current,
+         description,
+      } = req.body;
+
+      const newEducation = {
+         school,
+         degree,
+         fieldofstudy,
+         from,
+         to,
+         current,
+         description,
+      };
+
+      try {
+         const profile = await Profile.findOne({ user: req.user.id });
+
+         profile.education.unshift(newEducation);
+         await profile.save();
+
+         res.json(profile);
+      } catch (err) {
+         console.error(err);
+         res.status(500).send('Server Error');
+      }
+   }
+);
+
+// @route       DELETE api/profile/education/:edu_id
+// @desc        Delete education from user profile
+// @access      Private
+
+router.delete('/education/:edu_id', auth, async (req, res) => {
+   try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      // Get the remove index
+      const removeIndex = profile.education
+         .map((item) => item.id)
+         .indexOf(req.params.edu_id);
+
+      profile.education.splice(removeIndex, 1);
+
+      await profile.save();
+
+      res.json(profile);
+   } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+   }
+});
+
+// @route       GET api/profile/github/:username
+// @desc        Get user repositories from GitHub
+// @access      Profile
+
+router.get('/github/:username', async (req, res) => {
+   try {
+      const options = {
+         uri: `https://api.github.com/users/${
+            req.params.username
+         }/repos?per_page=5&
+         sort=created:asc&client_id=${config.get(
+            'githubClientId'
+         )}&client_secret=$
+         {config.get('githubSecret')}`,
+         method: 'GET',
+         headers: { 'user-agent': 'node.js' },
+      };
+
+      request(options, (error, response, body) => {
+         if (error) {
+            console.error(error);
+         }
+         if (response.statusCode !== 200) {
+            return res.status(404).json({ msg: 'No GitHub profile found' });
+         }
+
+         res.json(JSON.parse(body));
+      });
+   } catch (err) {
+      console.error(err);
+      res.status(500).send('Server Error');
+   }
+});
 module.exports = router;
