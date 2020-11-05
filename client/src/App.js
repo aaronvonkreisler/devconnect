5958d2ea7b6ac7bd391c84495848223cd19a60e8
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2020-11-05 15:11:47 -0600

    set up redux actions and reducers

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { Container } from '@material-ui/core';
+
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
@@ -10,20 +13,22 @@ import Register from './components/auth/Register';
 
 const App = () => {
    return (
-      <Router>
-         <Fragment>
-            <CssBaseline>
-               <Navbar />
-               <Route exact path="/" component={Landing} />
-               <Container style={{ marginTop: '2rem' }}>
-                  <Switch>
-                     <Route exact path="/register" component={Register} />
-                     <Route exact path="/login" component={Login} />
-                  </Switch>
-               </Container>
-            </CssBaseline>
-         </Fragment>
-      </Router>
+      <Provider store={store}>
+         <Router>
+            <Fragment>
+               <CssBaseline>
+                  <Navbar />
+                  <Route exact path="/" component={Landing} />
+                  <Container style={{ marginTop: '2rem' }}>
+                     <Switch>
+                        <Route exact path="/register" component={Register} />
+                        <Route exact path="/login" component={Login} />
+                     </Switch>
+                  </Container>
+               </CssBaseline>
+            </Fragment>
+         </Router>
+      </Provider>
    );
 };
 
