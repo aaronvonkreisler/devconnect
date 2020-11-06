1a0f7f7ea59d746beb98f3b9b7f146d9ee57847b
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2020-11-05 18:48:03 -0600

    client side auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
@@ -12,7 +12,18 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
 
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+
+if (localStorage.token) {
+   setAuthToken(localStorage.token);
+}
+
 const App = () => {
+   useEffect(() => {
+      store.dispatch(loadUser());
+   }, []);
+
    return (
       <Provider store={store}>
          <Router>
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,8 +1,11 @@
 import React, { Fragment, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Link, Redirect } from 'react-router-dom';
 import { TextField, Button } from '@material-ui/core';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import { makeStyles } from '@material-ui/core/styles';
+import { login } from '../../actions/auth';
 
 const useStyles = makeStyles((theme) => ({
    root: {
@@ -15,7 +18,7 @@ const useStyles = makeStyles((theme) => ({
    },
 }));
 
-const Login = () => {
+const Login = ({ login, isAuthenticated }) => {
    const classes = useStyles();
    const [formData, setFormData] = useState({
       email: '',
@@ -29,8 +32,13 @@ const Login = () => {
 
    const onFormSubmit = async (e) => {
       e.preventDefault();
-      console.log('Success');
+      login(email, password);
    };
+
+   // Redirect if log in successful
+   if (isAuthenticated) {
+      return <Redirect to="/dashboard" />;
+   }
 
    return (
       <Fragment>
@@ -75,4 +83,13 @@ const Login = () => {
    );
 };
 
-export default Login;
+Login.propTypes = {
+   login: PropTypes.func.isRequired,
+   isAuthenticated: PropTypes.bool,
+};
+
+const mapStateToProps = (state) => ({
+   isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps, { login })(Login);
