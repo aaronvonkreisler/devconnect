f636bbaec2194061bccd16be0f14c6436f513268
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2020-11-20 08:16:49 -0600

    display github repo cards on profile

--- client/src/components/profilev2/github/GithubLayout.js
+++ client/src/components/profilev2/github/GithubLayout.js
@@ -1,21 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { makeStyles, Grid } from '@material-ui/core';
 import { getGithubRepos } from '../../../actions/profile';
-const GithubLayout = ({ getGithubRepos }) => {
+import GithubItem from './GithubItem';
+
+const useStyles = makeStyles((theme) => ({
+   root: {
+      marginTop: theme.spacing(3),
+   },
+}));
+
+const GithubLayout = ({ username, repos, getGithubRepos }) => {
+   const classes = useStyles();
+
+   useEffect(() => {
+      getGithubRepos(username);
+   }, [getGithubRepos, username]);
+
    return (
       <div>
-         <h2>Github Layou</h2>
+         {repos === null ? (
+            <p>'Loading' </p>
+         ) : (
+            <div className={classes.root}>
+               <Grid container spacing={1}>
+                  {repos.map((repo) => (
+                     <GithubItem repo={repo} key={repo.id} />
+                  ))}
+               </Grid>
+            </div>
+         )}
       </div>
    );
 };
 
 GithubLayout.propTypes = {
    getGithubRepos: PropTypes.func.isRequired,
+   repos: PropTypes.array.isRequired,
+   username: PropTypes.string.isRequired,
 };
 
 const mapStateToProps = (state) => ({
-   repos: state.repos,
+   repos: state.profile.repos,
 });
 
 export default connect(mapStateToProps, { getGithubRepos })(GithubLayout);
