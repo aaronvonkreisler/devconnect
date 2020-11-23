f8a6388ce409a2fbe5a4f947ac54ecbf886142f8
Aaron von Kreisler <aaronvonkreisler@gmail.com>
2020-11-23 10:06:33 -0600

    removed sass

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,13 @@
 import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
+
 import store from './store';
 
 import './App.css';
-import './styles/_app.scss';
+
 import Alert from './components/layout/Alert';
-
 import Dashboard from './components/dashboard/Dashboard';
-
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Navigation from './components/layout/Navigation';
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,7 +2,14 @@ import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { Link, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { TextField, Button, Grid, Card, Typography } from '@material-ui/core';
+import {
+   TextField,
+   Button,
+   Grid,
+   Card,
+   Typography,
+   CssBaseline,
+} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 import { setAlert } from '../../actions/alert';
@@ -50,84 +57,90 @@ const Register = ({ setAlert, register, isAuthenticated }) => {
    }
 
    return (
-      <div className="signup flex flex-center w-100 h-100vh">
-         <div className="p-8">
-            <Card className="signup-card position-relative y-center">
-               <Grid container>
-                  <Grid item lg={5} md={5} sm={5} xs={12}>
-                     <div className="p-32 flex flex-center flex middle h-100">
-                        <img src="assets/illustrations/standing-4.svg" alt="" />
-                     </div>
-                  </Grid>
-                  <Grid item lg={7} md={7} sm={7} xs={12}>
-                     <div className="p-36 h-100 bg-light-gray position-relative">
-                        <Typography variant="h5">Sign Up Today</Typography>
-                        <form
-                           className={classes.root}
-                           onSubmit={(e) => onFormSubmit(e)}
-                        >
-                           <div>
-                              <TextField
-                                 fullWidth
-                                 label="Name"
-                                 type="text"
-                                 variant="outlined"
-                                 name="name"
-                                 value={name}
-                                 onChange={(e) => onTextChange(e)}
-                              />
-                           </div>
-                           <div>
-                              <TextField
-                                 fullWidth
-                                 label="Email Address"
-                                 type="email"
-                                 variant="outlined"
-                                 name="email"
-                                 value={email}
-                                 onChange={(e) => onTextChange(e)}
-                              />
-                           </div>
-                           <div>
-                              <TextField
-                                 fullWidth
-                                 label="Password"
-                                 type="password"
-                                 variant="outlined"
-                                 name="password"
-                                 value={password}
-                                 onChange={(e) => onTextChange(e)}
-                              />
-                           </div>
-                           <div>
-                              <TextField
-                                 fullWidth
-                                 label="Confirm Password"
-                                 type="password"
-                                 variant="outlined"
-                                 name="password2"
-                                 value={password2}
-                                 onChange={(e) => onTextChange(e)}
-                              />
-                           </div>
-                           <Button
-                              type="submit"
-                              color="primary"
-                              variant="contained"
+      <React.Fragment>
+         <CssBaseline />
+         <div className="signup flex flex-center w-100 h-100vh">
+            <div className="p-8">
+               <Card className="signup-card position-relative y-center">
+                  <Grid container>
+                     <Grid item lg={5} md={5} sm={5} xs={12}>
+                        <div className="p-32 flex flex-center flex middle h-100">
+                           <img
+                              src="assets/illustrations/standing-4.svg"
+                              alt=""
+                           />
+                        </div>
+                     </Grid>
+                     <Grid item lg={7} md={7} sm={7} xs={12}>
+                        <div className="p-36 h-100 bg-light-gray position-relative">
+                           <Typography variant="h5">Sign Up Today</Typography>
+                           <form
+                              className={classes.root}
+                              onSubmit={(e) => onFormSubmit(e)}
                            >
-                              Sign Up
-                           </Button>
-                        </form>
-                        <Typography variant="subtitle1" display="block">
-                           Already have an account?{' '}
-                           <Link to="/login">Sign In</Link>
-                        </Typography>
-                     </div>
+                              <div>
+                                 <TextField
+                                    fullWidth
+                                    label="Name"
+                                    type="text"
+                                    variant="outlined"
+                                    name="name"
+                                    value={name}
+                                    onChange={(e) => onTextChange(e)}
+                                 />
+                              </div>
+                              <div>
+                                 <TextField
+                                    fullWidth
+                                    label="Email Address"
+                                    type="email"
+                                    variant="outlined"
+                                    name="email"
+                                    value={email}
+                                    onChange={(e) => onTextChange(e)}
+                                 />
+                              </div>
+                              <div>
+                                 <TextField
+                                    fullWidth
+                                    label="Password"
+                                    type="password"
+                                    variant="outlined"
+                                    name="password"
+                                    value={password}
+                                    onChange={(e) => onTextChange(e)}
+                                 />
+                              </div>
+                              <div>
+                                 <TextField
+                                    fullWidth
+                                    label="Confirm Password"
+                                    type="password"
+                                    variant="outlined"
+                                    name="password2"
+                                    value={password2}
+                                    onChange={(e) => onTextChange(e)}
+                                 />
+                              </div>
+                              <Button
+                                 type="submit"
+                                 color="primary"
+                                 variant="contained"
+                              >
+                                 Sign Up
+                              </Button>
+                           </form>
+                           <Typography variant="subtitle1" display="block">
+                              Already have an account?{' '}
+                              <Link to="/login">Sign In</Link>
+                           </Typography>
+                        </div>
+                     </Grid>
                   </Grid>
-               </Grid>
-            </Card>
+               </Card>
+            </div>
          </div>
-      </div>
+      </React.Fragment>
    );
 };
 
